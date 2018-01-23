9566486324d621ac11cd43e71f650819af08ec0a
Henry Ward <henry.neil.ward@gmail.com>
2018-01-23 12:58:46 -0600

    Added script

---
+++ slack_to_bib.R
@@ -0,0 +1,156 @@
+#!/usr/bin/env Rscript
+
+suppressWarnings(library(knitcitations))
+suppressWarnings(library(httr))
+suppressWarnings(library(jsonlite))
+
+######
+# MAIN FUNCTIONS
+######
+
+# Gets messages in a specified date range from slack channel
+get_slack_messages <- function(token, channel_id, latest = "now",
+                               oldest = 0) {
+  response <- POST(url="https://slack.com/api/channels.history",
+                   body=list(token=token,
+                             channel=channel_id,
+                             count=1000,
+                             oldest=oldest))
+  message_table <- fromJSON(content(response, as="text"))$messages
+  return(message_table)
+}
+
+# Adds bib entry to bibtex file, creating it if nonexistent
+write_bib <- function(bib_file, append = TRUE) {
+  if (!file.exists(bib_file)) {
+    write.bibtex(file = bib_file)
+  } else {
+    write.bibtex(file = bib_file, append = append)
+  }
+}
+
+# Filters messages into a list of valid DOIs
+messages_to_doi <- function(messages) {
+  messages$text <- tolower(messages$text)
+  dois <- gsub("[\r\n]", " ", messages$text)
+  dois <- unlist(strsplit(dois, " "))
+  dois <- dois[grepl("doi", dois)]
+  dois <- dois[grepl("\\d", dois)]
+  dois <- filter_doi(dois)
+  return(dois)
+}
+
+# Cleans dois
+filter_doi <- function(doi) {
+  doi <- gsub("<", "", doi)
+  doi <- gsub(">", "", doi)
+  doi <- gsub(",", "", doi)
+  doi <- gsub("\"", "", doi)
+  doi <- gsub("\'", "", doi)
+  doi <- gsub("doi:", "", doi)
+  return(doi)
+}
+
+# Turns a doi string into a bibtex citation
+doi_to_bib <- function(doi) {
+  
+  # Parses string prefix
+  if (substr(doi, 1, 5) == "https") {
+    doi <- substr(doi, 17, nchar(doi))
+  } else if (substr(doi, 1, 4) == "http") {
+    doi <- substr(doi, 17, nchar(doi))
+  }
+  
+  # Gets citation and stores in session memory
+  citep(doi)
+}
+
+# Manually appends a doi to a bib file
+append_doi <- function(doi, bib_file) {
+  doi <- filter_doi(doi)
+  if (substr(doi, 1, 5) == "https") {
+    doi <- substr(doi, 17, nchar(doi))
+  } else if (substr(doi, 1, 4) == "http") {
+    doi <- substr(doi, 17, nchar(doi))
+  }
+  citep(doi)
+  write_bib(bib_file, append = TRUE)
+}
+
+# Main function
+slack_to_bib <- function(slack_token_file, channel_id, bib_file,
+                         last_timestamp_file = NA, oldest = "all") {
+  
+  # Converts oldest to given range from now in seconds since epoch
+  if (oldest == "week") {
+    oldest = as.integer(as.POSIXct(Sys.time() - 604800)) 
+  } else if (oldest == "day") {
+    oldest = as.integer(as.POSIXct(Sys.time() - 86400)) 
+  } else if (oldest == "all") {
+    oldest = 0 
+  }
+  
+  # Loads timestamp from file if it exists, and writes timestamp to file
+  last_timestamp <- NA
+  if (!is.na(last_timestamp_file)) {
+    f <- file(last_timestamp_file)
+    if (file.exists(last_timestamp_file)) {
+      last_timestamp <- readLines(f)[[1]]
+    }
+    writeLines(as.character(Sys.time(), f))
+    close(f)
+  }
+  
+  # Overwrites oldest with timestamp if specified
+  if (!is.na(last_timestamp)) {
+    oldest <- as.POSIXct(last_timestamp)
+  }
+  
+  # Gets slack messages
+  token <- readLines(slack_token_file)[[1]]
+  messages <- get_slack_messages(token, channel_id, oldest = oldest)
+  
+  # Gets all DOIs in messages
+  dois <- messages_to_doi(messages)
+  
+  # Resets knitcitations citation list and sets format to pandoc
+  cleanbib()
+  options("citation_format" = "pandoc")
+  
+  # Writes dois to file
+  for (doi in dois) {
+    doi_to_bib(doi)
+  }
+  write_bib(bib_file)
+}
+
+######
+# COMMAND LINE INTERFACE
+######
+
+# Gets and parses args
+args <- commandArgs()
+options = list(
+  make_option(c("-t", "--token_file"), type="character", default=NULL, 
+              help="path to file containing slack API token"),
+  make_option(c("-c", "--channel_id"), type="integer", default=NULL, 
+              help="target slack channel id"),
+  make_option(c("-b", "--bib_file"), type="character", default="citations.bib", 
+              help="path to bibtex file to output or update [default= %default]"),
+  make_option(c("-l", "--last_timestamp_file"), type="character", default="timestamp.txt", 
+              help="path to file containing the last timestamp [default= %default]"),
+  make_option(c("-o", "--oldest"), type="character", default="week", 
+              help="range of days to check slack history for [default= %default]")
+)
+
+# Extracts variables from args
+token_file <- opt$token_file
+channel_id <- opt$channel_id
+bib_file <- opt$bib_file
+last_timestamp_file <- opt$last_timestamp_file
+oldest <- opt$oldest
+
+# Calls main function
+slack_to_bib(token_file, channel_id, bib_file,
+             last_timestamp_file, oldest)
+
